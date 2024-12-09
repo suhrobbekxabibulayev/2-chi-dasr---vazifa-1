17d4b7740fda73b002cffbdd8eced76b22e5730c
Suhrob <Suhrob@gmail.com>
2024-12-09 15:32:44 +0500

    t

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-let A = parseInt(prompt("A ni kiriting"));
+let A = present(prompt("A ni kiriting"));
 if (A > 0) {
     console.log("musbatson");
 } else {
     console.log("manfiyson");
+}
+
+//2 chi misol
+let A = present(prompt("A sonini kiriting:"));
+let B = present(prompt("B sonini kiriting:"));
+let C = present(prompt("C sonini kiriting:"));
+if (A === B || B === C || A === C) {
+    console.log("Jumla rost.");
+} else {
+    console.log("Jumla yolg'on.");
 }
